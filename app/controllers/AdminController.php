7115df775982757d189f2eb1888194cfeba6e3f4
Wassim El Mourabit <wassimelmorabit7@gmail.com>
2025-01-08 16:55:47 +0100

    Changing clients status

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,51 +1,54 @@
-<?php 
-require_once (__DIR__.'/../models/User.php');
-require_once  (__DIR__."/../models/Admin.php");
+<?php
+require_once(__DIR__ . '/../models/User.php');
+require_once(__DIR__ . "/../models/Admin.php");
 
-class AdminController extends BaseController {
-    private $AdminModel ;
-    public function __construct(){
+class AdminController extends BaseController
+{
+    private $AdminModel;
+    public function __construct()
+    {
 
         $this->AdminModel = new Admin();
-  
-        
-     }
+    }
 
-   public function index() {
-      
-      if(!isset($_SESSION['user_loged_in_id'])){
-         header("Location: /login ");
-         exit;
-      }
-    $this->renderAdmin('index');
-   }
-   
+    public function index()
+    {
 
-   public function getAllUsers(){
-      $users = $this->AdminModel->getUsers();
-      $this->renderAdmin('clients', ['users' => $users]);
-   }
+        if (!isset($_SESSION['user_loged_in_id'])) {
+            header("Location: /login ");
+            exit;
+        }
+        $this->renderAdmin('index');
+    }
+
+    public function compte()
+    {
+        // Your code here
+        return $this->renderAdmin('compte');
+    }
+
+    public function transactions()
+    {
+        // Your code here
+        return $this->renderAdmin('transactions');
+    }
+    public function dashboard()
+    {
+        // Your code here
+        return $this->renderAdmin('index');
+    }
+
+    public function getAllUsers()
+    {
+        $users = $this->AdminModel->getUsers();
+        $this->renderAdmin('clients', ['users' => $users]);
+    }
 
 
-
-
-   public function compte()
-   {
-       // Your code here
-       return $this->renderAdmin('compte');
-   }
-   
-   public function transactions()
-   {
-       // Your code here
-       return $this->renderAdmin('transactions');
-   }
-   public function dashboard()
-   {
-       // Your code here
-       return $this->renderAdmin('index');
-   }
-
- 
-
+    public function changeStatus($userId, $status)
+    {
+        $this->AdminModel->changeStatus($userId, $status);
+        header("Location: /clients");
+        exit;
+    }
 }
